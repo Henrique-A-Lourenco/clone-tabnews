0b81b0bf967c905f57169160e62cb894fb949db6
Henrique A. Lourenço <91293974+Henrique-A-Lourenco@users.noreply.github.com>
2024-02-02 18:18:06 +0000

    Adicionando pagina 'em construção' ao projeto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import styles from "../styles/Home.module.css";
+
 function Home() {
-  return <h1>Se quiser sim mano...</h1>;
+  return (
+    <div className={styles.container}>
+      <img src="/images/LogoDev.png" alt="Logo Dev" className={styles.image} />
+      <h1 className={styles.title}>Página em construção</h1>
+      <p className={styles.text}>
+        Esse projeto é para colocar em pratica os conhecimento adquiridos no{" "}
+        <b>curso.dev</b>.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
