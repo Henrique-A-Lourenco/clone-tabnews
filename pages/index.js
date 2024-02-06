2bb56322873683725807370f610a37f7aba6fce0
Henrique A. Lourenço <91293974+Henrique-A-Lourenco@users.noreply.github.com>
2024-02-06 02:34:18 +0000

    Adiconando informações a pagina

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,29 @@
 import styles from "../styles/Home.module.css";
+import Head from "next/head";
 
 function Home() {
   return (
     <div className={styles.container}>
-      <img src="/images/LogoDev.png" alt="Logo Dev" className={styles.image} />
+      <Head>
+        <title>Em Construção</title>
+        <link rel="icon" href="/images/icon.png" />
+      </Head>
+      <h1>Isso mesmo não tem nada pronto </h1>
+      <img
+        src="/images/SeQuiserSimMano.gif"
+        alt="Logo Dev"
+        className={styles.image}
+      />
       <h1 className={styles.title}>Página em construção</h1>
       <p className={styles.text}>
-        Esse projeto é para colocar em pratica os conhecimento adquiridos no{" "}
-        <b>curso.dev</b>.
+        Esse projeto é para colocar em prática os conhecimento adquiridos no
+        <b> curso.dev</b>.<br></br>
+        <a
+          href="https://github.com/Henrique-A-Lourenco/clone-tabnews"
+          target="_blank"
+        >
+          <b>link GitHub</b>
+        </a>
       </p>
     </div>
   );
